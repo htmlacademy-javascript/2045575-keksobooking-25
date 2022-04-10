47791f8e6bf4bda38c2de68dfeb1a9bcaf2583ff
Matvei <mateodenisous69@gmail.com>
2022-04-10 13:44:08 +0300

    cachatsa

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-// import './form.js';
 import './map.js';
-import { showAlert } from './util.js';
-import { getData } from './server-api.js';
-// const MAX_ADS_AMOUNT = 10;
-
-// const pepe = (ads) => {
-//   ads.slice(0, MAX_ADS_AMOUNT).forEach(({offer, author, location}) => {
-//     putMarkerOnMap({offer, author, location});
-//   });
-// };
-
-// getData(pepe, () => showAlert('Не удалось загрузить данные с сервера.'));
